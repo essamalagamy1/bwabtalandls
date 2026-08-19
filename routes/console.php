1ae67e6b90d414a5e09835dbc461cbd346f5b9f4
Essam Hamdy AL-agamy <20912018100106@fci.zu.edu.eg>
2026-08-19 20:27:43 +0300

    feat: schedule queue worker to run every minute without overlapping

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:deactivate-expired')->daily();
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
